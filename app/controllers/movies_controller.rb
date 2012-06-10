a6ec153ad3b17a8b4aa3ab361b8f26c577180f4a
Noel del Castillo <ngdelcastillo@gmail.com>
2012-06-10 18:35:47 +0800

    added sortable columns

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort])
+    @sort_by = params[:sort]
   end
 
   def new
